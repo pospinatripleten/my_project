d092db7eb3546104b1ba5f3d5672ad065aef3204
unknown <litaospina121@hotmail.com>
2025-06-03 21:58:00 -0500

    se agrego checkbox

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
  
 autos= pd.read_csv('C:\\Users\\Paola\\Myprojects\\my_project\\vehicles_us.csv',sep=',') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
- 
+hist_button2 = st.checkbox('Grafico de Dispersión') # crear un botón
 if hist_button: # al hacer clic en el botón
          # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -14,3 +14,14 @@ if hist_button: # al hacer clic en el botón
      
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+ 
+if hist_button2: # al hacer clic en el botón
+         # escribir un mensaje
+    st.write('Creación de un grafico de dispersión')
+         
+    # crear un histograma
+    figb = px.scatter(autos, x="odometer", y="price")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(figb, use_container_width=True)
