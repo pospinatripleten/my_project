83e56103c777f687582c6b62217db26c085a85df
unknown <litaospina121@hotmail.com>
2025-06-03 22:41:00 -0500

    cambio ruta

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my_project
 My Test Project
+  Local URL: http://localhost:8501
+  Network URL: http://192.168.80.11:8501
+
+https://github.com/pospinatripleten/my_project.git
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
  
-autos= pd.read_csv('C:\\Users\\Paola\\Myprojects\\my_project\\vehicles_us.csv',sep=',') # leer los datos
+autos= pd.read_csv('vehicles_us.csv',sep=',') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 hist_button2 = st.checkbox('Grafico de Dispersión') # crear un botón
 if hist_button: # al hacer clic en el botón
@@ -20,7 +20,7 @@ if hist_button2: # al hacer clic en el botón
          # escribir un mensaje
     st.write('Creación de un grafico de dispersión')
          
-    # crear un histograma
+    # crear un dispersion
     figb = px.scatter(autos, x="odometer", y="price")
      
     # mostrar un gráfico Plotly interactivo
